7c4cf95d3c9091a127c1df05e59cb0e4a5003dba
goldmaster631026 <goldmaster631026@gmail.com>
2024-11-28 09:23:40 +1000

    server.js

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.use(morgan("dev"))
 app.get("/", (req, res) => {
  res.status(200).json({msg:"⏳ Server is running"})
 })
-//commit23
+//commit
 app.use("/api", routes)
 
 app.listen(PORT, console.log(`⏳ Server is Running at https://${HOST}/${PORT}`))
